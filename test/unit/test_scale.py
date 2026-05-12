a94cfdda85e272d6abc4c356b93b935b5281f181
tjlane <thomas.joseph.lane@gmail.com>
2026-05-12 13:06:35 -0400

    one small value

--- test/unit/test_scale.py
+++ test/unit/test_scale.py
@@ -404,5 +404,5 @@ def test_scalar_scale_reciprocal_vs_real_space(random_difference_map: Map, multi
     m1.canonicalize_amplitudes()
     rescaled_m2.canonicalize_amplitudes()
 
-    np.testing.assert_allclose(m1.amplitudes, rescaled_m2.amplitudes, rtol=0.01)
-    np.testing.assert_allclose(m1.phases, rescaled_m2.phases, rtol=0.05)
+    np.testing.assert_allclose(m1.amplitudes, rescaled_m2.amplitudes, rtol=0.01, atol=0.01)
+    np.testing.assert_allclose(m1.phases, rescaled_m2.phases, rtol=0.01, atol=0.01)
